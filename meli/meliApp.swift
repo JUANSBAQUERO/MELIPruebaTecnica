fd87ed4903c8e0e1ad16ea251d9daef2b28d7a93
Juan <mac@macs-MBP.lan>
2025-02-14 10:46:46 -0500

    Initial commit

---
+++ meli/meliApp.swift
@@ -0,0 +1,17 @@
+//
+//  meliApp.swift
+//  meli
+//
+//  Created by Juan  on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct meliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meliTests/meliTests.swift
@@ -0,0 +1,16 @@
+//
+//  meliTests.swift
+//  meliTests
+//
+//  Created by Juan  on 14/02/25.
+//
+
+import Testing
+
+struct meliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
